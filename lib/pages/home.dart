fb7c8880303eb3d2ff61db9833887a01b6281c5c
w05163 <979350927@qq.com>
2020-03-02 00:19:50 +0800

    增加微博详情页

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,21 +5,36 @@ import 'package:xany/models/home.dart';
 import 'package:xany/widgets/weibo.dart';
 
 class HomePage extends StatelessWidget {
+  toDetail(BuildContext context, int index) {
+    Navigator.pushNamed(context, '/weiboDetail', arguments: {'index': index});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<HomeModel>(
-        builder: (context, HomeModel home, _) => Scaffold(
-            appBar: null,
-            body: new ListView.builder(
-              itemCount: home.weibos.length,
-              itemBuilder: (context, index) {
-                return Weibo(home.weibos[index]);
-              },
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: home.getNextPage,
-              tooltip: 'Increment Counter',
-              child: const Icon(Icons.refresh),
-            )));
+      builder: (context, HomeModel home, _) => Scaffold(
+        appBar: null,
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(231, 231, 231, 1),
+          ),
+          child: ListView.builder(
+            itemCount: home.weibos.length,
+            itemBuilder: (context, index) {
+              Map weibo = home.weibos[index];
+              return InkWell(
+                onTap: () => toDetail(context, index),
+                child: Hero(tag: weibo['id'], child: Weibo(weibo)),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: home.getNextPage,
+          tooltip: 'Increment Counter',
+          child: const Icon(Icons.refresh),
+        ),
+      ),
+    );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 import 'pages/index.dart';
+import 'pages/weiboDetail.dart';
 
 final routes = <String, WidgetBuilder>{
   '/': (BuildContext context) => InitPage(),
   '/login': (BuildContext context) => LoginPage(),
   '/home': (BuildContext context) => HomePage(),
+  '/weiboDetail': (BuildContext context) => WeiboDetail(),
 };
